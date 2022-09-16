59279b41b3eb3216b027bc68ea40a605769aa822
JuliFabiani <julianmatias00@hotmail.com>
2022-09-16 00:54:05 -0300

    Redirección a listaDeClientes

--- src/app/components/crear-usuario/crear-usuario.component.ts
+++ src/app/components/crear-usuario/crear-usuario.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ClienteModel } from './cliente.model';
 import { NgForm } from '@angular/forms';
 import { APIService } from 'src/app/services/api.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-crear-usuario',
@@ -12,18 +13,22 @@ import { APIService } from 'src/app/services/api.service';
 export class CrearUsuarioComponent implements OnInit {
 
   cliente = new ClienteModel();
+  cargando:boolean = false;
 
-  constructor(private clienteService:APIService) {  }
+  constructor(private clienteService:APIService, private router:Router) {  }
   ngOnInit(): void {  }
 
   guardar(form:NgForm) {
     if(form.invalid) return console.log('Formulario inválido.');
 
     console.log('Request enviado.');
-    let startFrom = new Date().getTime();      
+    let startFrom = new Date().getTime();
+    this.cargando = true;      
     this.clienteService.crearCliente(this.cliente).subscribe(resp => { 
       // console.log(resp);
       console.log(`Tiempo de respuesta: ${new Date().getTime() - startFrom} milisegundos.`);
+      this.router.navigate(['listaDeUsuarios']);
+      this.cargando = false;
     });
   }
 }
